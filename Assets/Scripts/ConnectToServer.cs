5241ab3ca223d67764e38f1cd02d9068fdf17848
deadheart123 <shreyas2799@gmail.com>
2022-12-12 21:22:43 +0000

    Setting up Loading Scene and Connection
    Setting up Loading Scene and Connection to the Photon PUN server. Added a Loading scene for transition.

---
+++ Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    
+    void Start()
+    {
+        //Establish connection of Player and Photon Server
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Connected to the Photon Server");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();        
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+}
