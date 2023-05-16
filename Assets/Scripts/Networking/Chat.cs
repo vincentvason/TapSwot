cff14bc37b07a8e4a513de09014c28a1064787b7
deadheart123 <shreyas2799@gmail.com>
2023-05-16 13:45:19 +0100

    [FINAL] POST FIXES AND TEXT CORRECTIONS
    [FINAL] POST FIXES AND TEXT CORRECTIONS

--- Assets/Scripts/Networking/Chat.cs
+++ Assets/Scripts/Networking/Chat.cs
@@ -11,13 +11,59 @@ public class Chat : MonoBehaviour
     public TMP_InputField inputField;
     public GameObject Message;
     public GameObject Content;
+    public GameObject chatArea;
+    public GameObject notification;
+ 
+    private int messageCount;
+    private void Start()
+    {
+        messageCount = 0;
+    }
+    public void Update()
+    {
+       if ( Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (chatArea.activeInHierarchy == false)
+            {
+                chatArea.SetActive(true);
+            }
+            else
+            {
+                chatArea.SetActive(false);
+            }
+        }
 
+       if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(chatArea.activeInHierarchy == true)
+            {
+                if (inputField.text != "")
+                {
+                    SendMessage();
+                }
+            }
+        }
+        if (Content.transform.childCount > messageCount && chatArea.activeInHierarchy == false)
+        {
+     
+                notification.SetActive(true);
+        
+      
+        }   
+        if(Content.transform.childCount > messageCount && chatArea.activeInHierarchy == true)
+        {
+            notification.SetActive(false);
+            messageCount = Content.transform.childCount;
+        }
+ 
+    }
 
     public void SendMessage()
     {
         GetComponent<PhotonView>().RPC("GetMessage", RpcTarget.All,PhotonNetwork.NickName + " : " + inputField.text);
 
         inputField.text = "";
+      
     }
 
     [PunRPC]
